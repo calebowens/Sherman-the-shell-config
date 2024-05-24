ad55361ad160df71c03a62824fd264e4d16e75d2
Caleb Owens <hello@calebowens.com>
2024-05-24 16:11:31 +0200

    asdfadfs

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,7 @@
 echo "Loading aliases"
 
 # Tmuxinator
+alias tmuxinator='$HOME/.rbenv/versions/3.3.0/bin/tmuxinator'
 alias tx='tmuxinator'
 
 # Docker aliases
@@ -13,3 +14,10 @@ alias dcr="docker compose run"
 alias rip="rip --graveyard ~/.local/share/Trash"
 
 alias vim="nvim"
+
+alias notes="vim ~/Notes"
+
+
+asdf
+
+asdf
