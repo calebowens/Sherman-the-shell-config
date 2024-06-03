476f225a98f673029219861a382517d0cbf0ae7e
Caleb Owens <hello@calebowens.com>
2024-06-03 17:27:37 +0200

    afsafds

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 echo "Starting sherman"
 
 
-source $HOME/sherman-the-shell-config/constants.sh
+source $HOME/code/sherman-the-shell-config/constants.sh
 source $SHERMAN/utils.sh
-source $SHERMAN/sherman_ruby.sh
+# source $SHERMAN/sherman_ruby.sh
 source $SHERMAN/reload.sh
 source $SHERMAN/aliases.sh
 source $SHERMAN/private_aliases.sh
